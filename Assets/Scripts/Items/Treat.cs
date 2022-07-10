36a5b75301d79b3e14c81353190a0f4638ba4663
lyricnrhyme <jamieyamashita.12@gmail.com>
2022-07-09 21:32:06 -1000

    Implemented Gain Life animation when Player touches cat treats

--- Assets/Scripts/Items/Treat.cs
+++ Assets/Scripts/Items/Treat.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Treat : MonoBehaviour
 {
     GameManager gameManager;
+    Animator kittenMovementAnim;
 
     void Start() {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        kittenMovementAnim = GameObject.Find("Kitten").GetComponent<Animator>();
     }
 
     void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.tag == Constants.PLAYER_TAG) {
+            kittenMovementAnim.SetTrigger("GainLife");
             gameManager.AddLife();
         }
 
