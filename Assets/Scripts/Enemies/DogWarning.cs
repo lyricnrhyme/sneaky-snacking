0bc8e987373b2c63f6157ca196f1072451aa6dca
lyricnrhyme <jamieyamashita.12@gmail.com>
2022-07-13 11:46:59 -1000

    Added in visual indicator for timers when dog and human show up

--- Assets/Scripts/Enemies/DogWarning.cs
+++ Assets/Scripts/Enemies/DogWarning.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DogWarning : MonoBehaviour {
     public GameObject dog;
+    public Image radialTimer;
+    float secondsLeft = 3f;
 
     private void OnEnable () {
         StartCoroutine (ShowDog ());
-        Debug.Log("SHOW ME PLS");
+    }
+
+    private void Update () {
+        if (secondsLeft > 0) {
+            secondsLeft -= Time.deltaTime;
+            radialTimer.fillAmount = secondsLeft / 3f;
+        }
     }
 
     IEnumerator ShowDog () {
-        yield return new WaitForSeconds (3f);
+        yield return new WaitForSeconds (secondsLeft);
         dog.SetActive (true);
         gameObject.SetActive (false);
+        secondsLeft = 3f;
+        radialTimer.fillAmount = 1;
     }
 }
--- Assets/Scripts/Enemies/HumanWarning.cs
+++ Assets/Scripts/Enemies/HumanWarning.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HumanWarning : MonoBehaviour
 {
     public GameObject human;
+    public Image radialTimer;
+    float secondsLeft = 3;
 
     private void OnEnable() {
         StartCoroutine(ShowHuman());
+    }
+
+    private void Update () {
+        if (secondsLeft > 0) {
+            secondsLeft -= Time.deltaTime;
+            radialTimer.fillAmount = secondsLeft / 3f;
+        }
     }
 
     IEnumerator ShowHuman() {
         yield return new WaitForSeconds(3f);
         human.SetActive(true);
         gameObject.SetActive(false);
+        secondsLeft = 3f;
+        radialTimer.fillAmount = 1;
     }
 }
