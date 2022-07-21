04021e6cdd67d2d2891dbd95984e228b44b75aa4
lyricnrhyme <jamieyamashita.12@gmail.com>
2022-07-20 15:21:40 -1000

    Added ability to load in character by selected index

--- Assets/Scripts/Game Management/CharacterManager.cs
+++ Assets/Scripts/Game Management/CharacterManager.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterManager : MonoBehaviour
 {
+    public GameObject[] kittenPlayers;
+    public Sprite[] pauseSprites;
+    public Sprite[] gameOverSprites;
+    public Sprite[] winSprites;
+    public Image pauseImage;
+    public Image gameOverImage;
+    public Image winImage;
+    int selectedCharacter;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Get playerprefs here after testing
+        selectedCharacter = PlayerPrefs.GetInt("SelectedCharacter", 0);
+        Instantiate(kittenPlayers[selectedCharacter], new Vector3(0f, -3.02f, 0f), Quaternion.identity);
+        pauseImage.sprite = pauseSprites[selectedCharacter];
+        gameOverImage.sprite = gameOverSprites[selectedCharacter];
+        winImage.sprite = winSprites[selectedCharacter];
     }
 }
